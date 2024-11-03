4d344f92b0a084717e2a29c1cc40d206071f8d24
Peter Hjelm <peter.hjelm1@gmail.com>
2024-11-03 15:59:27 +0100

    added the right code for creating a folder with users name

--- run_game.sh
+++ run_game.sh
@@ -2,12 +2,33 @@
 
 echo "$USER"s program
 
-mkdir -p "$USER"_labb
+full_name=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
 
-cp Guesser.java "$USER"_labb
-cp GuessingGame.java "$USER"_labb
+if [ -z "$full_name" ]; then
+    echo "Could not retrieve full name from system. Please enter your first and last name."
+    read -p "First Name: " first_name
+    read -p "Last Name: " last_name
+else
+    first_name=$(echo "$full_name" | awk '{print $1}')
+    last_name=$(echo "$full_name" | awk '{print $2}')
+fi
 
-cd "$USER"_labb
+if [ -z "$first_name" ] || [ -z "$last_name" ]; then
+    echo "It seems the full name could not be extracted automatically."
+    read -p "Please enter your first name: " first_name
+    read -p "Please enter your last name: " last_name
+fi
+
+dir_name="${first_name}_${last_name}"
+
+mkdir -p "$dir_name"_labb
+
+echo "Created directory: $dir_name"
+
+cp Guesser.java "$dir_name"_labb
+cp GuessingGame.java "$dir_name"_labb
+
+cd "$dir_name"_labb
 
 echo "Current directory: $(pwd)"
 
