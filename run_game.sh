5c990c8ed9547b06115e77f07f58cd7aef6e2dbf
Peter Hjelm <peter.hjelm1@gmail.com>
2024-11-04 10:21:01 +0100

    correcting to my name instead of teachers name

--- run_game.sh
+++ run_game.sh
@@ -1,34 +1,13 @@
 #!/bin/bash
 
-echo "$USER"s program
+echo Peter_Hjelms program
 
-full_name=$(getent passwd "$USER" | cut -d ':' -f 5 | cut -d ',' -f 1)
+mkdir -p Peter_Hjelm_labb
 
-if [ -z "$full_name" ]; then
-    echo "Could not retrieve full name from system. Please enter your first and last name."
-    read -p "First Name: " first_name
-    read -p "Last Name: " last_name
-else
-    first_name=$(echo "$full_name" | awk '{print $1}')
-    last_name=$(echo "$full_name" | awk '{print $2}')
-fi
+cp Guesser.java Peter_Hjelm_labb
+cp GuessingGame.java Peter_Hjelm_labb
 
-if [ -z "$first_name" ] || [ -z "$last_name" ]; then
-    echo "It seems the full name could not be extracted automatically."
-    read -p "Please enter your first name: " first_name
-    read -p "Please enter your last name: " last_name
-fi
-
-dir_name="${first_name}_${last_name}"
-
-mkdir -p "$dir_name"_labb
-
-echo "Created directory: $dir_name"
-
-cp Guesser.java "$dir_name"_labb
-cp GuessingGame.java "$dir_name"_labb
-
-cd "$dir_name"_labb
+cd Peter_Hjelm_labb
 
 echo "Current directory: $(pwd)"
 
